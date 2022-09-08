6c10381135868b9f5e81beb5cb1cbc57f77993c1
alejo9017 <alejo9017@gmail.com>
2022-09-07 22:31:34 -0500

    empieza el curso de ecmascript

---
+++ src/es6/00-let-cons.js
@@ -0,0 +1,23 @@
+var lastname = 'David';
+lastname = 'Oscar';
+console.log(lastname);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
+
+const fruits = () => {
+    if (true) {
+        var fruit1 = 'apple';
+        var fruit2 = 'Kiwi';
+        var fruit3 = 'Bananna';
+
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,12 @@
+function square(num) {
+    return num * num;
+}
+
+const square = (num) => {
+    return num * num;
+
+}
+
+const multipli = num => num * num;
+console.log(multipli(3));
+
